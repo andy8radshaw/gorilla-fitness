8607d79838efdf126edd7a75c2fd91715576942e
andy8radshaw <andy8radshaw@gmail.com>
2020-07-16 14:05:50 +0100

    registering and logging in working

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -5,5 +5,5 @@ class User(AbstractUser):
     email = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    profile_image = models.CharField(max_length=300)
-    bio = models.CharField(max_length=500)
+    profile_image = models.CharField(max_length=300, blank=True)
+    bio = models.CharField(max_length=500, blank=True)

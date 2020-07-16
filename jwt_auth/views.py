687a109c46f5b9def57d91e67489f021053ff17b
andy8radshaw <andy8radshaw@gmail.com>
2020-07-16 13:01:07 +0100

    created user model. testing next

---
+++ jwt_auth/views.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from django.conf import settings
+import jwt
+
+from .serializers import UserSerializer
+User = get_user_model()
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        created_user = UserSerializer(data=request.data)
+        if created_user.is_valid():
+            created_user.save()
+            return Response({'message': 'Registration successful'}, status=status.HTTP_201_CREATED)
+        return Response(created_user.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class LoginView(APIView):
+
+    def get_user(self, email):
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise PermissionDenied()
+
+    def post(self, request):
+        email = request.data.get('email')
+        password  = request.data.get('password')
+        user = self.get_user(email)
+        if not user.check_password(password):
+            raise PermissionDenied()
+        dt = datetime.now() + timedelta(days=7)
+        token = jwt.encode({
+            'sub': user.id,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY)
+        return Response({'token': token, 'message': f'Welcome back {user.username}'})
+
